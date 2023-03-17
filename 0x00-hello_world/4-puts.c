d8e3ba6886d636341be9d25c8cf0177560e6f80d
Natembeya <bramwellbarasa.com>
2023-03-17 03:58:46 -0700

    hello ,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
  * Return; always 0
  */
 int main(void)
-{ put("\ "Programming is like building a multilingual puzzle);
+{ puts("\ "Programming is like building a multilingual puzzle");
 	return(0);
 }
